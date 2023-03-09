757e44021ba9f6b94dd4ea87217915da523c5efe
Luciano <lucianoterres@gmail.com>
2023-03-09 17:15:23 -0300

    temp

---
+++ dir_local copy.py
@@ -0,0 +1,16 @@
+root =  f'E:\Luciano\_0PH\Datasets/' 
+masksDir =  root +  f'masks10-90'
+
+
+
+TRAIN_DIR  =  root + '/tgs-salt/'
+
+TRAIN_NOT_NULLCSV=TRAIN_DIR+'trainNOTNULL.csv'
+TRAIN_CSV = TRAIN_DIR + f'train.csv'  
+TRAIN1090_CSV = TRAIN_DIR + f'train1090.csv'    
+DEPTH_CSV = 'depths.csv'
+
+
+TRAIN_IMAGE_DIR = TRAIN_DIR +'/train/images/'
+TRAIN_MASK_DIR  = TRAIN_DIR +'/train/masks/'
+TEST_IMAGE_DIR  = TRAIN_DIR+ '/test/images/'
