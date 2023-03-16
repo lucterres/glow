d0596443b8f3686cc17d6531169e2fa179c5d2a3
Luciano <lucianoterres@gmail.com>
2023-03-16 12:07:44 -0300

    new samples and args
    new samples and args

---
+++ arg.py
@@ -0,0 +1,15 @@
+batch_size=200
+benchmark=True
+gpu_ids=[0]
+lr=1e-3
+max_grad_norm=-1.
+num_channels=512
+num_levels=3
+num_steps=32
+num_epochs=100
+num_samples=64
+num_workers=8
+resume=False
+seed=42
+warm_up=500000
+
