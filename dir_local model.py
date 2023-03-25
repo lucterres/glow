e5e00e3079876d4b38a3ad839f71d52f489181b2
Luciano Terres <lucianoterres@gmail.com>
2023-03-25 15:28:31 -0300

    model dir

--- dir_local model.py
+++ dir_local model.py
@@ -5,5 +5,8 @@ cifar=r"D:\_PHD\Dataset\cifar"
 TRAIN_NOT_NULLCSV = root + f"train.csv"
 TRAIN_IMAGE_DIR = root + f"test\images"
 
-TEST_NOT_NULLCSV = root + f"testNOTNULL.csv"
+
+#TEST train 18000
+TRAIN_NOT_NULLCSV = root + f"test\splitTrain.csv"
+TEST_NOT_NULLCSV = root + f"test\splitTest.csv"
 TEST_IMAGE_DIR   = root + f"test\images"
